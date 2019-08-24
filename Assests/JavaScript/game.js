e483883f824689ef70509b8a650cc1a3442284a4
Julia Ellboeck <juliaellboeck@gmail.com>
2019-08-23 22:08:21 -0400

    added event listener and cleaner up html

--- Assests/JavaScript/game.js
+++ Assests/JavaScript/game.js
@@ -1,12 +1,13 @@
-//Variables
 
-//Create the Array of words
+$(document).ready(function() {
 
-var wordList = ["acres", "adult" , "arrangement" , "advice" , "attempt" , "August" , "Autumn" , "border" , "breeze" , "brick" , "calm" , "canal", "cast", "chose" , "claws", "coach", "constantly" , "contrast" , "cookies" , "customs", "damage" ,"deeply", "depth", "discussion", "doll", "donkey", "Egypt", "essential" ];
+//Create the Array of words    
+var wordList = ["acres", "adult" , "arrangement" , "advice" , "attempt" , "August" , "Autumn" , "border" , "breeze" , "brick" , "calm" , "canal", "cast", "chose" , "claws", "coach", "constantly" , "contrast" , "cookies" , "customs", "damage" ,"deeply", "depth", "discussion", "doll", "donkey", "Egypt", "essential" ,"exchange" ,"exist", "explanation", "facing" , "film", "finest", "fireplace", "floating" ,"folks", "fort", "gargae", "grabbed","grandmonther","habit", "happily", "heading"];
 
 //Choosing the word from the array
-
 var word = wordList[Math.floor(Math.random() * wordList.length)];
+
+//Variables
 
 var length = word.length;
 var displayLength = [length]
@@ -19,25 +20,37 @@ console.log(length);
 
 //Create the underscores based on the length of the words
 
+// $(".start_btn").on("click", function() {
+
 for ( var i = 0; i <  word.length;  i++) {
     displayLength[i] = "_ ";
     displayAnswer= displayAnswer + displayLength[i];
-}
 
+}
+document.getElementById("answer").innerHTML = displayAnswer;
 console.log(displayAnswer)
 
-
-
-
-
-
-// //on click event to start the game
-
-// $(".start_btn").on("click", function() {
-
-
-    
 // });
+
+//user presses key and their guess is compared to the answer
+
+document.addEventListener("keyup", function() {
+
+    var userGuess = String.fromCharCode(event.keycode);
+    console.log(word.indexOf(userGuess));
+    if(word.indexOf(userGuess) > -1){
+        console.log(true);
+
+    }
+});
+
+
+
+
+
+
+});
+
 
 
 
