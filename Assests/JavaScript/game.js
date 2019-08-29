1d27627b69fd6eacb2fbf6346b12583d665b8c0a
Julia Ellboeck <juliaellboeck@gmail.com>
2019-08-28 20:59:28 -0400

    functioning js

--- Assests/JavaScript/game.js
+++ Assests/JavaScript/game.js
@@ -1,98 +1,90 @@
 //Create the Array of words    
-var wordList = ["acres", "adult" , "arrangement" , "advice" , "attempt" , "border" , "breeze" , "brick" , "calm" , "canal", "cast", "chose" , "claws", "coach", "constantly" , "contrast" , "cookies" , "customs", "damage" ,"deeply", "depth", "discussion", "doll", "donkey", "essential" ,"exchange" ,"exist", "explanation", "facing" , "film", "finest", "fireplace", "floating" ,"folks", "fort", "gargae", "grabbed","grandmother","habit", "happily", "heading"];
-//Choosing the word from the array
-var word = wordList[Math.floor(Math.random() * wordList.length)];
+var wordList = ["acres", "adult", "arrangement", "advice", "attempt", "border", "breeze", "brick", "calm", "canal", "cast", "chose", "claws", "coach", "constantly", "contrast", "cookies", "customs", "damage", "deeply", "depth", "discussion", "doll", "donkey", "essential", "exchange", "exist", "explanation", "facing", "film", "finest", "fireplace", "floating", "folks", "fort", "gargae", "grabbed", "grandmother", "habit", "happily", "heading"];
 
 //variables
-var length = word.length;
-var displayLength = [length]
-var guessesLeft = 10;
+
+var word = ""
+var guessesLeft = "";
 var userGuess = "";
 var displayAnswer = [];
 var incorrect_guess = [];
-var wordArray = word.split("");
-var lettersRemain= displayLength;
-var letterInWord =true;
-
-
-console.log(word)
-console.log(length);
-// console.log(wordArray);
 
 // function to start the game. Clears the variables and chooses a new word at random from the word array.
-
-function startGame () {
+startGame();
+function startGame() {
+    document.getElementById("message").innerHTML = "";
     guessesLeft = 10;
     incorrect_guess = [];
-    userGuess= "";
-    displayAnswer=[];
-     
-    }
+    word = randomWord();
+    console.log(word)
 
-document.getElementById("answer").innerHTML = displayAnswer;
-console.log(displayAnswer)
-document.getElementById("guess_remain").innerHTML = guessesLeft;
-document.getElementById("incorrect_guess").innerHTML = incorrect_guess;
+    document.getElementById("guess_remain").innerHTML = guessesLeft;
+    document.getElementById("incorrect_guess").innerHTML = incorrect_guess;
+    userGuess = "";
+    resetDisplay()
+}
 
-//user presses key and their guess is captured
+//Choosing the word from the array
 
-function display(){
+function randomWord() {
+    return wordList[Math.floor(Math.random() * wordList.length)];
+}
 
-    // loop displays the answer as underscores
-    
-    for ( var i = 0; i <  wordArray.length;  i++) {
-        displayAnswer.push(" _ ");
-        document.getElementById("answer").innerHTML = displayAnswer;
+//resets the display and pushes the underscores to the answer array
+function resetDisplay() {
+    displayAnswer = [];
+    document.getElementById("answer").innerHTML = "";
+
+    for (var i = 0; i < word.length; i++) {
+        displayAnswer.push("_");
+        document.getElementById("answer").innerHTML = displayAnswer.join(" ");
 
     }
 }
-    
-function checkLetter() {
 
-    display()
+//calls the start game function when the user clicks the button
+document.getElementById("startGame").addEventListener("click", startGame)
 
-            document.addEventListener("keyup", function() {
+//captures the users guess 
+document.addEventListener("keyup", function () {
 
-                var userGuess = event.key;
-                console.log(userGuess);
+    var userGuess = event.key;
+    userGuess = userGuess.toLowerCase();
+    console.log(userGuess);
 
-                for (var k = 0 ; k < word.length; k++){
-                    if (userGuess == word[k]){
-                        displayAnswer[k] = userGuess;
-                        letterInWord= true;
-                        document.getElementById("answer").innerHTML = displayAnswer.join(" ");
-                        
-        
-                    }
-   
-                    else { 
-                        letterInWord=false;
-                        console.log(letterInWord);
-                    }
-                }
-                
-                if (letterInWord)    {
-                    console.log(letterInWord);
-                   
-                    
-                } 
-                
-                else {
-                    guessesLeft--;
-                    incorrect_guess.push(userGuess);
-                    document.getElementById("incorrect_guess").innerHTML = incorrect_guess; 
-                    document.getElementById("guess_remain").innerHTML = guessesLeft;
-                }
-            
+    var letterInWord = false;
+    for (var k = 0; k < word.length; k++) {
+        if (userGuess === word[k].toLowerCase()) {
+            displayAnswer[k] = userGuess;
+            letterInWord = true;
+            document.getElementById("answer").innerHTML = displayAnswer.join(" ");
+        }
+    }
 
-                }) 
-         
-                }
+    if (!letterInWord) {
+        if (incorrect_guess.includes(userGuess)) {
+            console.log("Already Guessed")
+        } else {
+            guessesLeft--;
+            if(guessesLeft > 0){
+                incorrect_guess.push(userGuess);
+                document.getElementById("incorrect_guess").innerHTML = incorrect_guess.join(", ");
+                document.getElementById("guess_remain").innerHTML = guessesLeft;
+            } else {
+                document.getElementById("message").innerHTML = "You Lost!";
+                document.getElementById("guess_remain").innerHTML = guessesLeft;
+            }
+        }
 
-                  
-    
-           checkLetter()
+    } else {
+        if(!displayAnswer.includes("_")){
+            document.getElementById("message").innerHTML = "You Won!";
+        }
+    }
 
 
-          
- 
+})
+
+
+
+
